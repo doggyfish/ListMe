b1018c3317638f54574764fa90cf657d3f7aa4f4
Michael Ellis <xin228star@gmail.com>
2013-04-21 17:13:06 +1200

    updated listItem.js

--- ListMe/Scripts/app/itemList.js
+++ ListMe/Scripts/app/itemList.js
@@ -1,8 +1,39 @@
-﻿window.todoApp.itemListViewModel = (function (ko) {
-    var items = ko.observableArray([
-        { name: 'Car'},
-        { name: 'Bike'}
-    ]);
+﻿window.todoApp = window.todoApp || {};
+
+window.todoApp.itemListViewModel = (function (ko) {
+    var items = ko.observableArray([]);
+
+    (function getItemList() {
+        return ajaxRequest("get", "/api/listitem/")
+            .done(getSucceeded)
+            .fail(getFailed);
+
+        function getSucceeded(data) {
+            var mappedTodoLists = $.map(data, function (list) { return new { name: list.name } });
+            items(mappedTodoLists);
+        }
+
+        function getFailed() {
+            items([{ name: 'Failed1' }, { name: 'Failed2' }]);
+        }
+    })();
+
+    function ajaxRequest(type, url, data, dataType) { // Ajax helper
+        var options = {
+            dataType: dataType || "json",
+            contentType: "application/json",
+            cache: false,
+            type: type,
+            data: data ? data.toJson() : null
+        };
+        var antiForgeryToken = $("#antiForgeryToken").val();
+        if (antiForgeryToken) {
+            options.headers = {
+                'RequestVerificationToken': antiForgeryToken
+            }
+        }
+        return $.ajax(url, options);
+    }
 
     return {
         items: items
