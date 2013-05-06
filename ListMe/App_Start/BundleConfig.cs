2ea7e8ebd061d33df6d102bafa831171c884c092
Michael Ellis <xin228star@gmail.com>
2013-05-07 08:45:05 +1200

    Added bootstrap display on list

--- ListMe/App_Start/BundleConfig.cs
+++ ListMe/App_Start/BundleConfig.cs
@@ -18,6 +18,9 @@ namespace ListMe {
 			bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
 						"~/Scripts/knockout-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
+
 			bundles.Add(new ScriptBundle("~/bundles/ajaxlogin").Include(
 				"~/Scripts/app/ajaxlogin.js"));
 
@@ -38,7 +41,9 @@ namespace ListMe {
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 				"~/Content/Site.css",
-				"~/Content/TodoList.css"));
+				"~/Content/TodoList.css",
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-responsive.css"));
 
 			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
 						"~/Content/themes/base/jquery.ui.core.css",
